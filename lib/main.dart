dc36e48f1ef6ff53b5906a5269fca322117b3582
powerninja <asdyuy23@gmail.com>
2024-02-25 23:55:30 +0900

    仮組みでIdを指定してポケモンを取得する

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,8 +24,59 @@ class PokemonIllustrated extends StatefulWidget {
 }
 
 class _PokemonIllustratedState extends State<PokemonIllustrated> {
+  var _pokemonPicFront =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+
+  Future<void> fetchPokemon(String searchNum) async {
+    searchNum = searchNum.isEmpty ? '1' : searchNum;
+    Response res =
+        await Dio().get('https://pokeapi.co/api/v2/pokemon/${searchNum}/');
+    // print(res.data);
+    print(res.data['name']);
+    print(res.data['forms'][0]['url']);
+    Response res2 = await Dio().get(res.data['forms'][0]['url']);
+    print(res2.data['sprites']['front_default']);
+    _pokemonPicFront = res2.data['sprites']['front_default'];
+
+    // 画面を更新する
+    setState(() {});
+  }
+
+  //画面が表示された時に呼ばれる
+  @override
+  void initState() {
+    super.initState();
+    // 画像を取得する処理を呼び出す
+    fetchPokemon('1');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          decoration: const InputDecoration(
+            // 入力フォームの背景色
+            fillColor: Colors.white,
+            // 入力フォームの背景色を変更するためのプロパティ
+            filled: true,
+            // 入力フォームのヒントメッセージ
+            hintText: 'SearchNumPokemon',
+          ),
+          // キーボードのエンターキーを押した時の処理
+          onFieldSubmitted: (String value) {
+            // 画像を取得する処理を呼び出す
+            fetchPokemon(value);
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.network(_pokemonPicFront),
+          ],
+        ),
+      ),
+    );
   }
 }
